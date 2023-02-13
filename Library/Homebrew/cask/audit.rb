d87c03b7d47db95aec90d02a728f816ee54e0860
Markus Reiter <me@reitermark.us>
2023-02-13 21:15:59 +0100

    Add audit for versions containing colons or slashes.

--- Library/Homebrew/cask/audit.rb
+++ Library/Homebrew/cask/audit.rb
@@ -229,6 +229,18 @@ module Cask
     end
 
     sig { void }
+    def audit_version_special_characters
+      return unless cask.version
+
+      return if cask.version.latest?
+
+      raw_version = cask.version.raw_version
+      return unless raw_version.include?(":") || raw_version.include?("/")
+
+      add_error "version should not contains colons or slashes"
+    end
+
+    sig { void }
     def audit_no_string_version_latest
       return unless cask.version
 
